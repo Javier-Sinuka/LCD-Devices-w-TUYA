5b5fc96b18eedee57bdf83bfbaf624c6a1515b2f
Javier_Sinuka <javier_sinuka@mi.unc.edu.ar>
2024-05-29 20:16:02 -0300

    Agregado y modificacion de metodos

--- devices_info.py
+++ devices_info.py
@@ -41,12 +41,21 @@ def conversor_time_hours(time):
     return date
 
 """
-    Metodo que calcula el valor en milisegundos, realizando la resta de los dias ingresados
+    Metodo que calcula el valor en milisegundos, realizando la resta respecto al valor ingresado ('day' o 'hour')
     al valor en formato de milisegundos, retornando dicho valor en el mismo formato (milisegundos)
 """
-def calculate_previous_time(timestamp_ms, days_substract):
-    calulate_timestamp_ms = datetime.fromtimestamp(timestamp_ms/1000) - timedelta(days=days_substract)
-    return (int(calulate_timestamp_ms.timestamp() * 1000))
+def calculate_previous_time(timestamp_ms, substract, tipe:str):
+    calculate_timestamp_ms = 0
+    try:
+        if tipe == 'hour':
+            calculate_timestamp_ms = datetime.fromtimestamp(timestamp_ms / 1000) - timedelta(hours=substract)
+        elif tipe == 'day':
+            calculate_timestamp_ms = datetime.fromtimestamp(timestamp_ms / 1000) - timedelta(days=substract)
+        return (int(calculate_timestamp_ms.timestamp() * 1000))
+    except ValueError:
+        print("Ingrese valor correcto respecto al tipo a retornar: " + '\n' +
+              'day' + '\n' +
+              'hour' + '\n')
 
 """
     Metodo que devuelve una lista con los ID de los dispositivos y su customName asociado
@@ -100,10 +109,11 @@ def get_status_codes_device_list(device_id):
     Metodo que calcula el promedio de los los elementos ingresados por una lista y 
     devuelve dicho valor en formato entero (sin decimales)
 """
-def calulate_average(list: list):
+def calculate_average(list: list):
     average = 0
     for element in list:
-        average += element
+        if element is not None:
+            average += element
     return (int(average/len(list)))
 
 """
@@ -119,52 +129,79 @@ def calulate_average(list: list):
     :return
     Lista de elementos solicitados.
 """
-def get_status_report_log(device_id, code, size, start_time, end_time):
+def get_status_report_log(device_id, code, size: int, start_time: int, end_time: int):
     response = openapi.get("/v2.0/cloud/thing/{}/report-logs?codes={}&end_time={}&size={}&start_time={}".format(device_id, code, end_time, size, start_time)).get('result').get('logs')
     return response
 
-"""
-    Metodo que devuelve una lista con los elementos especificados por el 'code' ingresado.
-    Se puede especificar el tamano de la lista. Ademas, dichos elementos cuentan
-    con dos parametros: 'event_time': int, 'value': int.
-    
-    :params
-    device_id: str (ID del dispositivo),
-    code: str (Valor a obtener),
-    size: int (Cantidad de elementos a retornar en la lista),
-    start_time: int (En milisegundos, tiempo de inicio de la toma de datos),
-    end_time: int (En milisegundos, tiempo de finalizacion de la toma de datos).
-    
-    :return
-    Lista con elementos que contienen los siguientes parametros:
-        'event_time' -> Referencia al tiempo en que en que fue tomada la medicion (valor en milisegundos)
-        'value' -> Referencia al valor solicitado (code)
-    Dichos elementos seran los relacionados al codigo ingresado.
-"""
-def get_status_list(device_id, code, size: int, start_time: int, end_time: int):
-    status_list = []
-    return status_list
 
 """
-    Metodo que devuelve una lista con los elementos especificados por el 'code' ingresado.
-    Opciones validas de time_return:
-        day -> Devuelve una lista con 24 elementos que cuenta con los parametros 'event_time': int, 'value': int.
-        week -> Devuelve una lista con 7 elementos que cuenta con los parametros 'event_time': int, 'value': int.
-    Se toma el valor ingresado en end_time, y se le resta el valor correspondiente a la peticion (day=ultimas 24hs
-    medidas, week=ultimos 7 dias).    
-    
+    Metodo que devuelve una lista con 24 elementos especificados por el 'code' ingresado,
+    dichos valores son los equivalentes a 24hs antes del tiempo ingresado en 'end_time'.
+
     :params
     device_id: str (ID del dispositivo),
     code: str (Valor a obtener),
-    end_time: int (En milisegundos, tiempo de finalizacion de la toma de datos),
-    time_return: str (day -> Ultimas 24hs [lista de 24 elementos], week -> Ultimos 7 dias [lista de 7 elementos])
-    
+    end_time: int (En milisegundos, tiempo de finalizacion de la toma de datos)
+
     :return
     Lista con elementos que contienen los siguientes parametros:
         'event_time' -> Referencia al tiempo en que en que fue tomada la medicion (valor en milisegundos)
         'value' -> Promedio respecto al valor solicitado (code)
     Dichos elementos seran los relacionados al codigo ingresado.
 """
+
+
+def get_status_list_day(device_id, code, end_time: int):
+    status_list = []
+    for i in range(24):
+        new_end_time = calculate_previous_time(end_time, i, 'hour')
+        list_values = get_status_report_log(device_id, code, 100, calculate_previous_time(new_end_time, 1, 'hour'), new_end_time)
+        print(list_values)
+        # status_list.append()
+    return status_list
+
+# """
+#     Metodo que devuelve una lista con los elementos especificados por el 'code' ingresado.
+#     Se puede especificar el tamano de la lista (minimo 1 a  maximo de 100 elementos).
+#     Ademas, dichos elementos cuentan con dos parametros: 'event_time': int, 'value': int.
+#
+#     :params
+#     device_id: str (ID del dispositivo),
+#     code: str (Valor a obtener),
+#     size: int (Cantidad de elementos a retornar en la lista),
+#     start_time: int (En milisegundos, tiempo de inicio de la toma de datos),
+#     end_time: int (En milisegundos, tiempo de finalizacion de la toma de datos).
+#
+#     :return
+#     Lista con elementos que contienen los siguientes parametros:
+#         'event_time' -> Referencia al tiempo en que en que fue tomada la medicion (valor en milisegundos)
+#         'value' -> Referencia al valor solicitado (code)
+#     Dichos elementos seran los relacionados al codigo ingresado.
+# """
+# def get_status_list(device_id, code, size: int, start_time: int, end_time: int):
+#     status_list = []
+#     return status_list
+
+# """
+#     Metodo que devuelve una lista con los elementos especificados por el 'code' ingresado.
+#     Opciones validas de time_return:
+#         day -> Devuelve una lista con 24 elementos que cuenta con los parametros 'event_time': int, 'value': int.
+#         week -> Devuelve una lista con 7 elementos que cuenta con los parametros 'event_time': int, 'value': int.
+#     Se toma el valor ingresado en end_time, y se le resta el valor correspondiente a la peticion (day=ultimas 24hs
+#     medidas, week=ultimos 7 dias).
+#
+#     :params
+#     device_id: str (ID del dispositivo),
+#     code: str (Valor a obtener),
+#     end_time: int (En milisegundos, tiempo de finalizacion de la toma de datos),
+#     time_return: str (day -> Ultimas 24hs [lista de 24 elementos], week -> Ultimos 7 dias [lista de 7 elementos])
+#
+#     :return
+#     Lista con elementos que contienen los siguientes parametros:
+#         'event_time' -> Referencia al tiempo en que en que fue tomada la medicion (valor en milisegundos)
+#         'value' -> Promedio respecto al valor solicitado (code)
+#     Dichos elementos seran los relacionados al codigo ingresado.
+# """
 # def get_status_list_day_week(device_id, code, end_time: int, time_return: str):
 #     status_list = []
 #     try:
--- log_info.py
+++ log_info.py
@@ -2,18 +2,20 @@ import devices_info
 
 
 # # Seteo del ID del dispositivo
-# DEVICE_ID = devices_info.devices_list_id_and_custom_name()[0]['ID']
-# # print(DEVICE_ID)
-# start_time = devices_info.cloud_info_devices.get('t') - 604800000
-# # print(start_time)
-# start_time_prueba = devices_info.calculate_previous_time(devices_info.cloud_info_devices.get('t'), 7)
-# print('Start time prueba: ' + str(start_time_prueba))
+DEVICE_ID = devices_info.devices_list_id_and_custom_name()[0]['ID']
+print(DEVICE_ID)
+start_time = devices_info.cloud_info_devices.get('t') - 604800000
+# print(start_time)
+start_time_prueba = devices_info.calculate_previous_time(devices_info.cloud_info_devices.get('t'), 1, 'hour')
+print('Start time prueba: ' + str(start_time_prueba))
 # end_time = devices_info.cloud_info_devices.get('t')
-# print('End time: ' + str(end_time))
-# code = "cur_power"
-# actual_time = devices_info.conversor_time_hours(1716942775676)
-# print('Actual time: ' + str(actual_time))
-
+end_time = 1716935097862
+print('End time: ' + str(end_time))
+code = "cur_power"
+actual_time = devices_info.conversor_time_hours(1716942775676)
+print('Actual time: ' + str(actual_time))
+tiempo = devices_info.calculate_previous_time(1716935097862, 2, 'hour')
+print('Tiempo: ' + str(tiempo))
 # print('Info relevante: ' + '\n' +
 #       'Device ID: ' + DEVICE_ID + '\n' +
 #       'Conversor time - t valor: ' + conversor_time_hours(response_test.get('t')) + '\n' +
@@ -22,6 +24,8 @@ import devices_info
 #       'End time: ' + str(end_time) + '\n' +
 #       'Actual time: ' + str(actual_time) + '\n'
 #       )
+
+devices_info.get_status_list_day(DEVICE_ID, code, end_time)
 
 # Metodo para traer los eventos de encendido y apagado
 # response = openapi.get("/v2.0/cloud/thing/{}/report-logs?codes={}&end_time={}&size=99&start_time={}".format(DEVICE_ID, code, end_time, start_time))
@@ -40,7 +44,7 @@ import devices_info
 
 # print(openapi.get("/v1.0/devices/live-datas?date_type=week&product_id={}".format(DEVICE_ID)))
 # print(devices_info.get_status_report_log(DEVICE_ID, code, 100, start_time, end_time))
-print(devices_info.calulate_average({23,23,22,25,23,23,25,26,99}))
+# print(devices_info.calulate_average({23,23,22,25,23,23,25,26,99}))
 
 # Metodo para ver la cantidad de dispositivos activos en el timepo indicado
 # ("Count the number of daily active devices" -> https://developer.tuya.com/en/docs/cloud/data-service?id=K95zu0f66bv4m#title-50-Count%20the%20number%20of%20daily%20active%20devices)
